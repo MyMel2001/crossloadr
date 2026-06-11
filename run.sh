f75bdd3af6c1949447dcd8db298832c4adc35c33
Mel <nope@example.com>
2026-06-10 23:14:24 -0500

    feat: add run.sh script to automate nvm setup, dependency installation, and background execution

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+nvm i 18
+nvm use 18
+
+npm i
+
+
+npm run start >crossloadr.log 2>&1 &
