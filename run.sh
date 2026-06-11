4402c82269db5938705d63fe79e5d279898a5afd
Mel <nope@example.com>
2026-06-10 23:33:57 -0500

    nvm use lts

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-nvm i 18
-nvm use 18
+nvm i --lts
+nvm use --lts
 
 npm i
 
